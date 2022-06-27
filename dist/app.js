e06c6ab8230085f5ea19141d7002eceb7508c3f4
cmjiwa <wangjinghuan@yahoo.com>
2022-06-27 16:50:08 +0300

    another generic function. create a custom type her an interface for T to extend

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,16 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: "rose" }, { age: 6 });
 console.log("mergedObj = ", mergedObj);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements.`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("hello rose"));
+console.log(countAndDescribe([2, 1, 4, 5, 6]));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -21,3 +21,21 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 }
 const mergedObj = merge({ name: "rose" }, { age: 6 });
 console.log("mergedObj = ", mergedObj);
+
+//another generic function
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements.`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("hello rose")); //["hello rose", "Got 10 elements."]
+console.log(countAndDescribe([2, 1, 4, 5, 6])); //[[2, 1, 4, 5, 6], "Got 5 elements."]
