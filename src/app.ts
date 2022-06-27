88cfc6e495e701288f4026ea50f1617649039d9b
cmjiwa <wangjinghuan@yahoo.com>
2022-06-27 15:25:30 +0300

    final codes for generic function

--- src/app.ts
+++ src/app.ts
@@ -18,13 +18,7 @@
 function merge<T, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
-const mergedObj = merge<{ name: string }, { age: number }>(
-  { name: "rose" },
-  { age: 6 }
-);
-const mergedObj2 = merge<{ name: string }, number[]>(
-  { name: "rose" },
-  [1, 2, 3]
-);
+const mergedObj = merge({ name: "rose" }, { age: 6 });
+const mergedObj2 = merge({ name: "rose" }, [1, 2, 3]);
 console.log("mergedObj = ", mergedObj);
 console.log("mergedObj2 = ", mergedObj2); //{0: 1, 1: 2, 2: 3, name: "rose"}
