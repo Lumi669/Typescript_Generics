5b88e4123eb085d5ea57976502c49585460b9fd4
cmjiwa <wangjinghuan@yahoo.com>
2022-06-27 15:22:05 +0300

    generic function

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,8 @@
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: "rose" }, { age: 90 });
+const mergedObj = merge({ name: "rose" }, { age: 6 });
+const mergedObj2 = merge({ name: "rose" }, [1, 2, 3]);
 console.log("mergedObj = ", mergedObj);
+console.log("mergedObj2 = ", mergedObj2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -13,16 +13,18 @@
 //   data.split(" ");
 // });
 
-//demonstration  a generic function
-//using type casting to get rid of error on Line31
-function merge(objA: object, objB: object) {
-  return Object.assign(objA, objB) as { name: string; age: number };
+//demonstration generic function
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
-
-// use <> version of type casting also works
-// function merge(objA: object, objB: object) {
-//   return <{ name: string; age: number }>Object.assign(objA, objB);
-// }
-
-const mergedObj = merge({ name: "rose" }, { age: 90 });
+const mergedObj = merge<{ name: string }, { age: number }>(
+  { name: "rose" },
+  { age: 6 }
+);
+const mergedObj2 = merge<{ name: string }, number[]>(
+  { name: "rose" },
+  [1, 2, 3]
+);
 console.log("mergedObj = ", mergedObj);
+console.log("mergedObj2 = ", mergedObj2); //{0: 1, 1: 2, 2: 3, name: "rose"}
